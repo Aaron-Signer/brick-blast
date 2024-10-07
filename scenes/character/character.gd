6df4d85c9d6f8a8aef85c5d16808772a76a2ac34
Aaron-Signer <aaronsigner1995@gmail.com>
2024-10-06 21:48:42 -0400

    Start screen dissapears on start and reapears when round ends

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -7,8 +7,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_just_pressed("start")):
-		Signals.launch_ball.emit()
+	pass
 
 func _physics_process(delta):
 	if (Input.is_action_pressed("left")):
--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -2,23 +2,28 @@ extends Node2D
 
 @export var ball: PackedScene
 @export var start_game_label: PackedScene
+var start_game_label_node: Node
+var game_in_progress: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	restart()
+	end_game()
 	Signals.launch_ball.connect(start)
-	Signals.ball_off_screen.connect(restart)
+	Signals.ball_off_screen.connect(end_game)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (!game_in_progress && Input.is_action_just_pressed("start")):
+		Signals.launch_ball.emit()
 
 func start():
-	start_game_label.queue_free()
+	game_in_progress = true
+	start_game_label_node.queue_free()
 	var ball = ball.instantiate()
 	add_child(ball)
 	
-func restart():
-	var label = start_game_label.instantiate()
-	add_child(label)
+func end_game():
+	game_in_progress = false
+	start_game_label_node = start_game_label.instantiate()
+	add_child(start_game_label_node)
