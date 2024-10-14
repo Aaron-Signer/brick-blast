2fe606002ef76722e8b156c2aeaac78c21ecd9fd
Aaron-Signer <aaronsigner1995@gmail.com>
2024-10-14 16:58:32 -0400

    Ball launches at a random angle

--- scenes/game_container/game/ball/ball.gd
+++ scenes/game_container/game/ball/ball.gd
@@ -6,7 +6,9 @@ func _ready():
 	linear_damp_mode = 1
 	linear_damp = 0
 	gravity_scale = 0
-	apply_central_impulse(Vector2(0,-400))
+	var launch_angle = randf_range(-PI/8, PI/8)
+	var vector = Vector2(0, -400).rotated(launch_angle)
+	apply_central_impulse(vector)
 
 
 func _process(delta):
