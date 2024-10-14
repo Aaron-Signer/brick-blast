3bc7e1a65a284bfa239bff022b164acafb67776c
Aaron-Signer <aaronsigner1995@gmail.com>
2024-10-14 12:28:33 -0400

    Added Brick Blast Rich Label Text in main menu

--- scenes/game_container/main_menu/level_select/level_select.gd
+++ scenes/game_container/main_menu/level_select/level_select.gd
@@ -1,11 +1,19 @@
-extends Control
-
+extends GridContainer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_level_1_button_pressed():
+	Signals.level_selected.emit("1")
+
+
+func _on_level_2_button_pressed():
+	Signals.level_selected.emit("2")
